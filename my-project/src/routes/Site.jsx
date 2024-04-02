1d52e90ceb5ecd684ae6d8531e906ae1552b49a3
Mirton Marques <m-marques-gomes@hotmail.com>
2024-04-02 16:57:17 -0300

    carrossel começando

--- my-project/src/routes/Site.jsx
+++ my-project/src/routes/Site.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import './Site.css';
+import tecnologia from '../img/tecnologia.png'
+import images from '../img/images.jpeg'
+import tec from '../img/tec.jpeg'
 
 function Site() {
   return (
@@ -60,6 +63,28 @@ function Site() {
           </div>
         </div>
       </nav>
+
+      <div id="carouselExampleFade" className="carousel slide carousel-fade">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={tecnologia} className="d-block w-100" alt={tecnologia} />
+          </div>
+          <div className="carousel-item">
+            <img src={images} className="d-block w-100" alt={images} />
+          </div>
+          <div className="carousel-item">
+          <img src={tec} className="d-block w-100" alt={tec} />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </>
   );
 }
