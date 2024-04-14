08e8027d842cb9b682b9cd0d7ec485622d053ee8
mirtongomes <mirton.gomes@aspec.com.br>
2024-04-14 00:59:54 -0300

    sidebar da home criado

--- my-project/src/Pages/Home.jsx
+++ my-project/src/Pages/Home.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import '../styles/Home.css'; 
 import logo from '../img/logoaspec.png';
+import '../styles/SidebarHome.css';
 
 function App() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message: ''
+    password: ''
   });
+
+  const [showSidebar, setShowSidebar] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,38 +23,47 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+
     setFormData({
       name: '',
       email: '',
-      password: '' // Limpe também o campo 'password'
+      password: ''
     });
+  };
+
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar); // Toggle sidebar visibility
   };
 
   return (
     <>
-     {/* Navbar */}
-     <nav className="navbar">
-     <div className="logo">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo">
           <a href="#">
             <img src={logo} alt="Logo" />
           </a>
         </div>
-         <div className="titulo">TEC</div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">Página Principal</a>
-          </li>
-          <li>
-            <a href="#">Sobre</a>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-          </li>
-        </ul>
+        <div className="titulo" onClick={toggleSidebar}>TEC</div>
+        {/* Botão para abrir o sidebar */}
+        <button className="sidebar-toggle" onClick={toggleSidebar}>
+          Menu
+        </button>
       </nav>
-    
+
+      <aside className={`sidebar ${showSidebar ? 'open' : ''}`}>
+       
+        <div className="sidebar-content">
+          <button >Registre-se</button>
+          <button >Quem Somos</button>
+          <button >Contatos</button>
+        </div>
+      </aside>
+
+      {/* Formulário */}
       <div className="form-container">
-      <h3>Registre-se</h3>
+        <h3>Registre-se</h3>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Nome</label>
@@ -59,12 +71,12 @@ function App() {
               type="text"
               id="name"
               name="name"
-              value={formData.usuario}
+              value={formData.name}
               onChange={handleChange}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="email">Sobrenome</label>
+            <label htmlFor="email">E-mail</label>
             <input
               type="email"
               id="email"
@@ -74,7 +86,7 @@ function App() {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="senha">E-mail</label>
+            <label htmlFor="password">Senha</label>
             <input
               type="password"
               id="password"
@@ -83,17 +95,6 @@ function App() {
               onChange={handleChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-
           <div className="form-group">
             <button type="submit">Cadastrar</button>
           </div>
