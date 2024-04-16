0556a97a0f1b956e11bed46ab8f50e0e032d6a2c
mirtongomes <mirton.gomes@aspec.com.br>
2024-04-16 19:31:05 -0300

    form ok

--- my-project/src/Pages/Login.jsx
+++ my-project/src/Pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
       setUsername('');
       setPassword('');
-    }      
+    }
 
 
   };
@@ -34,31 +34,35 @@ const Login = () => {
     <div className="bglight">
       <div className="login">
         <form className="login-form" onSubmit={handleLogin}>
-          <label htmlFor="username"></label>
           <h1>Aspec</h1>
-          <input
-            className="email"
-            type="text"
-            id="username"
-            placeholder="Email"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
+          <div className='form'>
+            <label htmlFor="username"></label>
+            <input
+              className="email"
+              type="text"
+              id="username"
+              placeholder="Email"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+         
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+
           <button type="submit">Login</button>
-        </form>
-        <div className='SingUp'> 
-        <strong>Does'n have an account? <a href="">Sing Up</a></strong>
-        <br />
-        <strong>Forgot <a href="">Username / Password?</a></strong>
+
+        <div className='Inscrever'>
+          <p>Does'n have an account? <a href="">Sing Up</a></p>
+          <p>Forgot <a href="">Username / Password?</a></p>
         </div>
+        </form>
       </div>
     </div>
   );
