709e19d88d394c39d229593e517ffd6ab31aa04f
mirtongomes <mirton.gomes@aspec.com.br>
2024-05-02 11:35:05 -0300

    atual

--- my-project/src/Pages/Site.jsx
+++ my-project/src/Pages/Site.jsx
@@ -1,33 +1,54 @@
 import React, { useState } from 'react';
-import '@/styles/Site.css';
 import { Link } from 'react-router-dom';
+import '@/styles/Site.css';
 
 const Site = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  // estado do sidebar
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para lidar com a pesquisa
+    console.log('Pesquisar:', searchQuery);
+  };
+
   return (
-    
     <div className={`bg-site ${sidebarOpen ? 'sidebar-open' : ''}`}>
-      {/*  btn menu */}
+      {/* Botão do menu */}
       <button className='toggle-btn' onClick={toggleSidebar}>
         <span className='menu-icon'>&#9776;</span>
       </button>
-      <div className='navbar'>
 
-      </div>
+      {/* Navbar */}
+      <nav className='navbar'>
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Search</button>
+          {/* Formulário de pesquisa */}
+          <form className="search-form" onSubmit={handleSearchSubmit}>
+          </form>
+        </div>
+      </nav>
 
       {/* Sidebar */}
       <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-        {/* Menu */}
         <ul>
-          <li>Item de Menu 1</li>
-          <li>Item de Menu 2</li>
-          <li>Item de Menu 3</li>
+          <li>Início</li>
+          <li>Usuários</li>
+          <li>Sair</li>
         </ul>
       </div>
     </div>
