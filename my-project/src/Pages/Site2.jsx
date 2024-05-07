e0752f05431c93ef848aa58b1c3ac136137c1e19
Mirton Marques <m-marques-gomes@hotmail.com>
2024-05-07 16:17:21 -0300

    ok

--- my-project/src/Pages/Site2.jsx
+++ my-project/src/Pages/Site2.jsx
@@ -26,47 +26,56 @@ const Site2 = () => {
 
   return (
     <>
-      <header>
-        <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
-        </div>
-        <div className="search-container">
-          <form onSubmit={handleSearchSubmit}>
-            <input
-              type="text"
-              placeholder="Buscar..."
-              value={searchQuery}
-              onChange={handleSearchChange}
-            />
-            <button type="submit">
-              <FiSearch />
-            </button>
-          </form>
-        </div>
-        <div className="icons-container">
-          <IoChatboxOutline />
-          <IoNotificationsOffOutline />
-        </div>
-        <div className="header-content">
-          <h2>Bem-vindo ao Meu Site</h2>
-          <p>Este é um exemplo de conteúdo no cabeçalho.</p>
-        </div>
-      </header>
+      <div className="bg-site2">
+        <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+          <aside class="list-sidebar">
+            <ul>
+              <li>
+                <a href="#">Página 1</a>
+              </li>
+              <li>
+                <a href="#">Página 2</a>
+              </li>
+              <li>
+                <a href="#">Página 3</a>
+              </li>
+            </ul>
+          </aside>
 
-      <aside class="sidebar2">
-        <ul>
-          <li><a href="#">Página 1</a></li>
-          <li><a href="#">Página 2</a></li>
-          <li><a href="#">Página 3</a></li>
-        </ul>
-      </aside>
+          <main>
+          <form className="form-site" onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Search projects..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              <div className="icons">
+                <button className='btn-lupa'>
+                  <FiSearch />
+                </button>
+                <button className='icon-msg'>
+                  <IoChatboxOutline />
+                </button>
+                <button className='icon-sino'>
+                  <IoNotificationsOffOutline />
+                </button>
+              </div>
+            </form>
+            <header className='cabecalho'>
+              <button className="toggle-btn" onClick={toggleSidebar}>
+                <span className="menu-icon">&#9776;</span>
+              </button>
+            </header>
 
-      <main>
-        <div className="main-content">
-          <h2>Conteúdo Principal</h2>
-          <p>Este é um exemplo de conteúdo no corpo do site.</p>
+            <div className="main-content">
+              <h2>Conteúdo Principal</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nobis iste excepturi a porro velit perferendis, necessitatibus placeat, molestiae eos quis qui autem recusandae voluptate sit voluptatem illum incidunt. Quos?</p>
+            </div>
+          </main>
+
         </div>
-      </main>
+      </div>
     </>
   );
 };
