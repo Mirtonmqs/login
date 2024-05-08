407d9fedfafe95611cb7ade86d81c0b35bced5bb
Mirton Marques <m-marques-gomes@hotmail.com>
2024-05-08 13:42:12 -0300

    alters

--- my-project/src/Pages/Site2.jsx
+++ my-project/src/Pages/Site2.jsx
@@ -42,37 +42,41 @@ const Site2 = () => {
             </ul>
           </aside>
 
-    <main>
-            <nav className='cabecalho'>
-            <form className="form-site" onSubmit={handleSearchSubmit}>
-              <input
-                type="text"
-                placeholder="Search projects..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-              />
-              <div className="icons">
-                <button className='btn-lupa'>
-                  <FiSearch />
-                </button>
-                <button className='icon-msg'>
-                  <IoChatboxOutline />
-                </button>
-                <button className='icon-sino'>
-                  <IoNotificationsOffOutline />
-                </button>
-              </div>
-            </form>
+          <main>
+            <nav className="cabecalho">
+              <form className="form-site" onSubmit={handleSearchSubmit}>
+                <input
+                  type="text"
+                  placeholder="Search projects..."
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+                <div className="icons">
+                  <button className="btn-lupa">
+                    <FiSearch />
+                  </button>
+                  <button className="icon-msg">
+                    <IoChatboxOutline />
+                  </button>
+                  <button className="icon-sino">
+                    <IoNotificationsOffOutline />
+                  </button>
+                </div>
+              </form>
               <button className="toggle-btn" onClick={toggleSidebar}>
                 <span className="menu-icon">&#9776;</span>
               </button>
             </nav>
-          </main>
-
             <div className="main-content">
               <h2>Conteúdo Principal</h2>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nobis iste excepturi a porro velit perferendis, necessitatibus placeat, molestiae eos quis qui autem recusandae voluptate sit voluptatem illum incidunt. Quos?</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium, nobis iste excepturi a porro velit perferendis,
+                necessitatibus placeat, molestiae eos quis qui autem recusandae
+                voluptate sit voluptatem illum incidunt. Quos?
+              </p>
             </div>
+          </main>
 
         </div>
       </div>
