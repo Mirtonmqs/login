0f544a0bd2d05a9d5536b8a7817ab6701d17f7d7
mirtongomes <mirton.gomes@aspec.com.br>
2024-05-07 23:06:08 -0300

    novo site

--- my-project/src/Pages/Site2.jsx
+++ my-project/src/Pages/Site2.jsx
@@ -26,54 +26,53 @@ const Site2 = () => {
 
   return (
     <>
-      <div className="bg-site2">
-        <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-          <aside class="list-sidebar">
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <div class="container">
+          <div class="sidebar2">
+            <div className=''>
+              <a href="#">
+                <img className='logo' src={logo} alt="Logo" />
+              </a>
+            </div>
             <ul>
-              <li>
-                <a href="#">Página 1</a>
-              </li>
-              <li>
-                <a href="#">Página 2</a>
-              </li>
-              <li>
-                <a href="#">Página 3</a>
-              </li>
+              <li>Início</li>
+              <li>Usuários</li>
+              <li>Sair</li>
             </ul>
-          </aside>
+          </div>
 
-    <main>
-            <nav className='cabecalho'>
-            <form className="form-site" onSubmit={handleSearchSubmit}>
-              <input
-                type="text"
-                placeholder="Search projects..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-              />
-              <div className="icons">
-                <button className='btn-lupa'>
-                  <FiSearch />
-                </button>
-                <button className='icon-msg'>
-                  <IoChatboxOutline />
-                </button>
-                <button className='icon-sino'>
-                  <IoNotificationsOffOutline />
+          <div class="cabecalho">
+            <div class="header">
+              <div className='btn-menu'>
+                <button className="toggle-btn" onClick={toggleSidebar}>
+                  <span className="menu-icon">&#9776;</span>
                 </button>
               </div>
-            </form>
-              <button className="toggle-btn" onClick={toggleSidebar}>
-                <span className="menu-icon">&#9776;</span>
-              </button>
-            </nav>
-          </main>
-
-            <div className="main-content">
-              <h2>Conteúdo Principal</h2>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nobis iste excepturi a porro velit perferendis, necessitatibus placeat, molestiae eos quis qui autem recusandae voluptate sit voluptatem illum incidunt. Quos?</p>
+              <form className="form-site" onSubmit={handleSearchSubmit}>
+                <input
+                  type="text"
+                  placeholder="Search projects..."
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+                <div className="icons">
+                  <button className='btn-lupa'>
+                    <FiSearch />
+                  </button>
+                  <button className='icon-msg'>
+                    <IoChatboxOutline />
+                  </button>
+                  <button className='icon-sino'>
+                    <IoNotificationsOffOutline />
+                  </button>
+                </div>
+              </form>
             </div>
 
+            <div class="body">
+              <h1>oi</h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
