026fe2e9b3c46e3827c85c326e86411761baeade
mirtongomes <mirton.gomes@aspec.com.br>
2024-05-09 12:06:10 -0300

    atual3

--- my-project/src/Pages/Site2.jsx
+++ my-project/src/Pages/Site2.jsx
@@ -28,11 +28,11 @@ const Site2 = () => {
   return (
     <>
       <div className={`${sidebarOpen ? 'open' : 'close'}`}>
-        <div class="container">
-          <div class="sidebar">
-            <div className="">
-              <a href="#">
-                <img className="logo" src={logo} alt="Logo" />
+        <div class='container'>
+          <div class='sidebar'>
+            <div className=''>
+              <a href='#'>
+                <img className='logo' src={logo} alt='Logo' />
               </a>
             </div>
             <ul>
@@ -42,34 +42,34 @@ const Site2 = () => {
             </ul>
           </div>
 
-          <div class="bg-site">
-            <div class="navbar">
-              <div className="btn-menu">
-                <button className="toggle-btn" onClick={toggleSidebar}>
-                  <span className="menu-icon">&#9776;</span>
+          <div class='bg-site'>
+            <div class='navbar'>
+              <div className='btn-menu'>
+                <button className='toggle-btn' onClick={toggleSidebar}>
+                  <span className='menu-icon'>&#9776;</span>
                 </button>
               </div>
-              <form className="form-site" onSubmit={handleSearchSubmit}>
+              <form className='form-site' onSubmit={handleSearchSubmit}>
                 <input
-                  type="text"
-                  placeholder="Search projects..."
+                  type='text'
+                  placeholder='Search projects...'
                   value={searchQuery}
                   onChange={handleSearchChange}
                 />
-                <div className="icons">
-                  <button className="btn-lupa">
+                <div className='icons'>
+                  <button className='btn-lupa'>
                     <FiSearch />
                   </button>
-                  <button className="icon-msg">
+                  <button className='icon-msg'>
                     <IoChatboxOutline />
                   </button>
-                  <button className="icon-sino">
+                  <button className='icon-sino'>
                     <IoNotificationsOffOutline />
                   </button>
                 </div>
               </form>
             </div>
-            <div className="corpo">
+            <div className='corpo'>
               <h1>oi</h1>
             </div>
           </div>
