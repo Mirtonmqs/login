b5c29b5a9b6a2af7826f1f7fbc3b5307de6c14c5
Mirton Marques <m-marques-gomes@hotmail.com>
2024-05-22 16:56:57 -0300

    dark mode começando

--- my-project/src/Pages/Site.jsx
+++ my-project/src/Pages/Site.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { IoNotificationsOffOutline, IoChatboxOutline } from 'react-icons/io5';
 import { CiLogout, CiUser, CiHome } from 'react-icons/ci';
+import { FaSun, FaMoon } from 'react-icons/fa'; // Importando os ícones para o modo claro e escuro
 import logo from '@/img/logoaspec.png';
 import perfil from '@/img/perfil.jpg';
 import '@/styles/Site.css';
@@ -10,10 +11,10 @@ import { Link } from 'react-router-dom';
 const Site2 = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
-    console.log('teste');
   };
 
   const handleSearchChange = (event) => {
@@ -25,86 +26,91 @@ const Site2 = () => {
     console.log('Pesquisar:', searchQuery);
   };
 
-  return (
-    <>
-      <div className={`${sidebarOpen ? 'open' : 'closed'}`}>
-        <div className="container">
-          <div className="sidebar">
-            <div className="topo-sidebar">
-              <a href="#">
-                <img className="logo" src={logo} alt="Logo" />
-              </a>
-            </div>
-            <div className="menu">
-              <button>
-                <Link to="/">
-                  <CiHome className="icon-home" aria-label="Início" />
-                  Início
-                </Link>
-              </button>
-              <button>
-                <Link to="/usuarios">
-                  <CiUser className="icon-user" aria-label="Usuários" />
-                  Usuários
-                </Link>
-              </button>
-              <button>
-                <Link to="/">
-                  <CiLogout className="icon-logout" aria-label="Sair" />
-                  Sair
-                </Link>
-              </button>
-            </div>
-          </div>
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-          <div className="bg-site">
-            <div className="navbar">
-              <div className="btn-menu">
-                <button className="toggle-btn" onClick={toggleSidebar}>
-                  <span className="menu-icon">&#9776;</span>
+  return (
+    <div className={`${sidebarOpen ? 'open' : 'closed'} ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+      <div className="container">
+        <div className="sidebar">
+          <div className="topo-sidebar">
+            <a href="#">
+              <img className="logo" src={logo} alt="Logo" />
+            </a>
+          </div>
+          <div className="menu">
+            <button>
+              <Link to="/">
+                <CiHome className="icon-home" aria-label="Início" />
+                Início
+              </Link>
+            </button>
+            <button>
+              <Link to="/usuarios">
+                <CiUser className="icon-user" aria-label="Usuários" />
+                Usuários
+              </Link>
+            </button>
+            <button>
+              <Link to="/">
+                <CiLogout className="icon-logout" aria-label="Sair" />
+                Sair
+              </Link>
+            </button>
+          </div>
+        </div>
+
+        <div className="bg-site">
+          <div className="navbar">
+            <div className="btn-menu">
+              <button className="toggle-btn" onClick={toggleSidebar}>
+                <span className="menu-icon">&#9776;</span>
+              </button>
+            </div>
+            <form className="form-site" onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Search projects..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              <div className="icons">
+                <button className="btn-lupa">
+                  <FiSearch />
+                </button>
+                <button className="icon-msg">
+                  <IoChatboxOutline />
+                </button>
+                <button className="icon-sino">
+                  <IoNotificationsOffOutline />
                 </button>
               </div>
-              <form className="form-site" onSubmit={handleSearchSubmit}>
-                <input
-                  type="text"
-                  placeholder="Search projects..."
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-                <div className="icons">
-                  <button className="btn-lupa">
-                    <FiSearch />
-                  </button>
-                  <button className="icon-msg">
-                    <IoChatboxOutline />
-                  </button>
-                  <button className="icon-sino">
-                    <IoNotificationsOffOutline />
-                  </button>
-                </div>
-                <div className="perfil">
-                  <img className="img-perfil" src={perfil} alt="perfil" />
-                  <div className="nome-usuario">Usuario</div>
-                </div>
-              </form>
-            </div>
-            <div className="corpo">
-              <h1 className="title-table">Tables</h1>
-              <div className="table-users">
-                <div className="text1">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-                    inventore maxime voluptatem assumenda, officiis ex corporis
-                    sint cupiditate praesentium quis distinctio dicta possimus
-                    cum laboriosam nulla, nisi deleniti. Corrupti, nisi?
-                  </p>
-                </div>
+              <div className="perfil">
+              <button className="toggle-dark-mode-btn" onClick={toggleDarkMode}>
+                {darkMode ? <FaSun /> : <FaMoon />} 
+              </button>
+                <img className="img-perfil" src={perfil} alt="perfil" />
+                <div className="nome-usuario">Usuario</div>
+              </div>
+            </form>
+          </div>
+          <div className="corpo">
+            <h1 className="title-table">Tables</h1>
+            <div className="table-users">
+              <div className="text1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                  inventore maxime voluptatem assumenda, officiis ex corporis
+                  sint cupiditate praesentium quis distinctio dicta possimus
+                  cum laboriosam nulla, nisi deleniti. Corrupti, nisi?
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
